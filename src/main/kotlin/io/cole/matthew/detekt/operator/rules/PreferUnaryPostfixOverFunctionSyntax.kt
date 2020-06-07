e714cbd17c7319fb5e57f1af17822c92cb641ffb
Matthew Cole <colematthew4@gmail.com>
2020-06-07 12:09:32 -0700

    Adds inc/dec postfix operator rule

---
+++ src/main/kotlin/io/cole/matthew/detekt/operator/rules/PreferUnaryPostfixOverFunctionSyntax.kt
@@ -0,0 +1,33 @@
+package io.cole.matthew.detekt.operator.rules
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Entity
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+import org.jetbrains.kotlin.util.OperatorNameConventions
+import org.jetbrains.kotlin.util.isValidOperator
+
+class PreferUnaryPostfixOverFunctionSyntax : PreferOperatorOverNamedFunctionSyntaxBase(
+    """
+        The unary postfix operators are referenced by their named function translation. 
+        This can be replaced by <value1>[(++)(--)].
+    """.trimIndent()
+) {
+    override fun visitCallExpression(expression: KtCallExpression) {
+        (expression.getResolvedCall(bindingContext)?.candidateDescriptor as? FunctionDescriptor)?.let { descriptor ->
+            if (descriptor.name != OperatorNameConventions.INC && descriptor.name != OperatorNameConventions.DEC) {
+                return
+            }
+
+            if (descriptor.isValidOperator()) {
+                report(CodeSmell(issue, Entity.from(expression), """
+                    The ${descriptor.fqNameOrNull()} method can be replaced with its unary postfix operator equivalent.
+                """.trimIndent()))
+            }
+        }
+
+        super.visitCallExpression(expression)
+    }
+}
